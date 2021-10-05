aa0df3c1faa873594ab03c8e6bde073fbf60eff2
sanographix <mail@sanographix.net>
2021-10-06 01:19:00 +0900

    カウントダウンタイマーの秒を追加・数字の値によってラベルを単数形にする

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -21,16 +21,40 @@
         period -= day * (1000 * 60 * 60 * 24);
         var hour = Math.floor(period / (1000 * 60 * 60));
         period -= hour * (1000 * 60 * 60);
-        var minutes = Math.floor(period / (1000 * 60));
-        period -= minutes * (1000 * 60);
+        var minute = Math.floor(period / (1000 * 60));
+        period -= minute * (1000 * 60);
+        var second = Math.floor(period / 1000);
         document.querySelector(".js-countdown-days").textContent = addZeroDay(day);
         document.querySelector(".js-countdown-hours").textContent = addZero(hour);
-        document.querySelector(".js-countdown-mins").textContent = addZero(minutes);
+        document.querySelector(".js-countdown-mins").textContent = addZero(minute);
+        document.querySelector(".js-countdown-secs").textContent = addZero(second);
+        // 数字が2以上のとき複数形にする
+        if (day > 1) {
+          document.querySelector(".js-label-days").textContent = 'Days';
+        } else {
+          document.querySelector(".js-label-days").textContent = 'Day';
+        }
+        if (hour > 1) {
+          document.querySelector(".js-label-hours").textContent = 'Hrs';
+        } else {
+          document.querySelector(".js-label-hours").textContent = 'Hr';
+        }
+        if (minute > 1) {
+          document.querySelector(".js-label-mins").textContent = 'Mins';
+        } else {
+          document.querySelector(".js-label-mins").textContent = 'Min';
+        }
+        if (second > 1) {
+          document.querySelector(".js-label-secs").textContent = 'Secs';
+        } else {
+          document.querySelector(".js-label-secs").textContent = 'Sec';
+        }
         setTimeout(countdownTimer, interval);
       } else {
         document.querySelector(".js-countdown-days").textContent = "000";
         document.querySelector(".js-countdown-hours").textContent = "00";
         document.querySelector(".js-countdown-mins").textContent = "00";
+        document.querySelector(".js-countdown-secs").textContent = "00";
       }
     }
     if (document.querySelector(".js-countdown")) {
